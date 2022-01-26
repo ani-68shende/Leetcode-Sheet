28616316b4953b216a670390b2799335a44c5db2
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-01-26 19:28:48 +0530

    Time: 24 ms (33.44%), Space: 16.5 MB (12.26%) - LeetHub

---
+++ 877-stone-game/877-stone-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>>dp(n , vector<int>(n));
+        for(int g = 0 ; g < n ; g++){
+            for(int i = 0, j = g ; j < n ; i++, j++){
+                if(g == 0){
+                    dp[i][j] = piles[i];
+                }else if(g == 1){
+                    dp[i][j] = max(piles[i], piles[j]);
+                }else{
+                    int val1 = piles[i]+min(dp[i+1][j-1], dp[i+2][j]);
+                    int val2 = piles[j]+ min(dp[i+1][j-1], dp[i][j-2]);
+                    dp[i][j] = max(val1, val2);
+                }
+            }
+        }
+        return dp[0][n-1]>0;
+    }
+};
