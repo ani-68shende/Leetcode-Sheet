1c512760697f423ce922eddb6f17ffac70b6f089
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-01-26 13:56:10 +0530

    Time: 3 ms (27.57%), Space: 6.4 MB (14.13%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int>ans;
+    void dfs(int low, int high, int i, int sum){
+        if(sum >= low && sum <= high){
+            ans.push_back(sum);
+        }
+        if(sum > high || i > 9){
+            return;
+        }
+        return dfs(low, high, i+1, sum*10+i);
+    }
+    vector<int> sequentialDigits(int low, int high) {
+        for(int i = 1 ; i <= 9 ; i++){
+            dfs(low, high, i, 0);
+        }sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
