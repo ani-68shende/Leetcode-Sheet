0611e34ac71d414a157555e55b71ff400af0c734
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-01-29 17:19:31 +0530

    Time: 39 ms (5.86%), Space: 15.7 MB (91.98%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        if (letters.back() <= target) return letters.front();
-        int low = 0, high = letters.size() - 1;
-        while (low < high) {
-            auto mid = (low + high) / 2;
-            if (target < letters[mid]) high = mid;
-            else low = mid + 1;
-        }
-        return letters[low];
+        //samajh ke bahar wala solution
+        
+        auto begin = letters.begin();
+        auto end = letters.end();
+        auto itr = upper_bound(begin, end, target);
+        return itr!=end ? *itr : *begin;
     }
 };
