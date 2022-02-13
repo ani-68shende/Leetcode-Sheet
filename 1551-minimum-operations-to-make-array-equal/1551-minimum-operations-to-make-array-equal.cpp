e5f35445a0a0493b92ea5e714ae622c8adca81a9
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-02-14 00:24:36 +0530

    Time: 112 ms (5.25%), Space: 5.9 MB (75.57%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -5,14 +5,13 @@ public:
         //1 3 5                 ->2
         //1 3 5 7 9             ->2+4 = 6
         //1 3 5 7 9 11          ->
-        vector<int>v;
-        for(int i = 0 ; i < n ; i++){
-            v.push_back(2*i+1);
-        }
         long long int sum = 0;
         for(int i = 0 ; i < n/2 ; i++){
-            sum += (-v[i]+v[n-i-1])/2;
+            int t = (-(2*i-1) + (2*(n-i-1)-1))/2;
+            if(t>0)sum +=t;
+            cout << sum << endl;
         }
+        cout << "----\n";
         return sum;
     }
 };
