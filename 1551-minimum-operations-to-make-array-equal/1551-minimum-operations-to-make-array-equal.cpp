7464a9d17d8b455a925765fead947613fec6a7e7
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-02-14 00:25:35 +0530

    Time: 6 ms (23.09%), Space: 5.9 MB (75.57%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -5,13 +5,11 @@ public:
         //1 3 5                 ->2
         //1 3 5 7 9             ->2+4 = 6
         //1 3 5 7 9 11          ->
+        // O(1) -> ofcourse space :D
         long long int sum = 0;
         for(int i = 0 ; i < n/2 ; i++){
-            int t = (-(2*i-1) + (2*(n-i-1)-1))/2;
-            if(t>0)sum +=t;
-            cout << sum << endl;
+            sum += (-(2*i-1) + (2*(n-i-1)-1))/2;
         }
-        cout << "----\n";
         return sum;
     }
 };
