5219e2281c2998aa0ee4d6e4b901a22dde1bada7
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-02-16 02:23:54 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (61.14%) - LeetHub

--- 925-long-pressed-name/925-long-pressed-name.cpp
+++ 925-long-pressed-name/925-long-pressed-name.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //finally all edge cases removed and solved successfully yay :D
     bool isLongPressedName(string name, string typed) {
         int flag = 0;
         unordered_map<char, int>mp,mp2;
@@ -7,27 +8,20 @@ public:
         int j = 0;
         int n = name.size();
         int m = typed.size();
-        // reverse(name.begin(), name.end());
-        // reverse(typed.begin(), typed.end());
         while(i < n && j < m){
             if(name[i]!=typed[j])return false;
             else{
-                cout << "First "<<  i << " " << j << " ";
                 char ch = typed[j];
                 i++;
                 j++;
                 while(i < n && j < m && name[i] == typed[j] && ch == name[i]){
                     i++; j++;
                 }
-                //cout << "j" << j << endl;
                 while(j < m && ch == typed[j]){
                     j++;
                 }
-                cout <<"Second " << i << " " << j << endl;
             }
         }
-        cout << i << " " << j << endl;
-        cout << n << " " << m << endl;
         if(i == n &&j == m )
         return true;
         else return false;
