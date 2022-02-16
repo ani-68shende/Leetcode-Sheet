48c3110a3ebc286ba847cfd502587e8e030b9c9e
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-02-16 16:13:32 +0530

    Time: 4 ms (72.12%), Space: 8.9 MB (50.91%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -7,7 +7,10 @@ public:
         if(nums[n-2]<nums[n-1])return n-1;
         int d=0;
         for(int i = 1 ; i< n-1 ; i++){
-            if(nums[i] > nums[i-1] && nums[i] > nums[i+1])return i;
+            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]){
+                d = i;
+                break;
+            }
         }
         return d;
     }
