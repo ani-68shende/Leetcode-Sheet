8437de24b9736e9e8bc8c957cb2ae9a74d8b6cdb
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-02-17 22:39:32 +0530

    Time: 88 ms (54.53%), Space: 11.5 MB (75.47%) - LeetHub

---
+++ 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkIfCanBreak(string s1, string s2) {
+        sort(s1.begin(), s1.end());
+        sort(s2.begin(), s2.end());
+        int flag1 = 0, flag2 = 0, n = s2.size();
+        for(int i = 0 ; i < n ; i++){
+            if(s1[i]< s2[i]){
+                flag1 = 1;
+            }
+        }
+        for(int i = 0 ; i < n ; i++){
+            if(s1[i]> s2[i]){
+                flag2 = 1;
+            }
+        }
+        if(flag1 == 1 && flag2 == 0)return true;
+        else if(flag2 == 1 && flag1 == 0)return true;
+        return false;
+    }
+};
