9c78763f7e80ff058f1c49f2952f20f5fe8e6092
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-02-19 10:23:46 +0530

    Time: 196 ms (74.66%), Space: 68.9 MB (27.55%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -3,14 +3,13 @@ public:
     int findJudge(int n, vector<vector<int>>& trust) {
         int z = trust.size();
         vector<int>degree(n+1,0);
-        unordered_map<int, int>zasu;
         for(auto x: trust){
-            zasu[x[0]]++;
+            degree[x[0]]--;
             degree[x[1]]++;
         }
         int x = *max_element(degree.begin(), degree.end());
         for(auto x = 1; x < degree.size() ; x++){
-            if(degree[x] == n-1 && zasu.find(x)==zasu.end())return x;
+            if(degree[x] == n-1)return x;
         }
         return -1;
     }
