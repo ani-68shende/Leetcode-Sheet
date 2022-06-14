5f434e5ace0d1def876ad76b2d6026b392778b52
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-06-14 08:43:00 +0530

    Time: 7 ms (52.47%), Space: 7.4 MB (85.21%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    // god or what method
+    
+    // a == b
+    // a = b
+    // a^b = b^b
+    // a^b = 0
+    //find all the substring length because the pivot can be any point in the substring so to count that we add the length of the substring each time we find the xor to be zero
     int countTriplets(vector<int>& arr) {
         int n = arr.size();
         int count = 0;
