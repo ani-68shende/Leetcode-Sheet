6b7df00ef1de00b9000fdc2f2bb59ab61fbf4afc
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-06-20 19:07:41 +0530

    Time: 701 ms (28.37%), Space: 108.2 MB (72.47%) - LeetHub

---
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>&wa) {
+        int n = wa.size();
+        int m = wa[0].size();
+        vector<vector<int>>v(n, vector<int>(m,0));
+        queue<pair<int, int>>q;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(wa[i][j] == 1){
+                    v[i][j] = 0;
+                    q.push({i, j});
+                }else{
+                    v[i][j] = -1;
+                }
+            }
+        }
+        vector<int>x{0,1,0,-1};
+        vector<int>y{1,0,-1,0};
+        while(!q.empty()){
+            auto fron = q.front();
+            q.pop();
+            int r = fron.first;
+            int c = fron.second;
+            for(int i = 0 ; i < 4 ; i++){
+                int nr = r+x[i];
+                int nc = c+y[i];
+                if(nr >=0 && nr < n && nc >= 0 && nc < m && v[nr][nc]==-1){
+                    v[nr][nc] = v[r][c]+1;
+                    q.push({nr, nc});
+                }
+            }
+        }
+        return v;
+    }
+};
