422dc694fbb77bc47c13d39bed38e61ecf3b5c91
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-06-23 10:50:32 +0530

    Time: 3 ms (43.26%), Space: 6.6 MB (25.31%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
-    //memoization (do not do dp[i][j]=1 || 0)
-    int f(int i, int j, vector<vector<int>>& dp){
-        if(i == 0 && j == 0)return 1;
-        if(i < 0 || j < 0){
-            return 0;
-        }
-        if(dp[i][j]!=-1)return dp[i][j];
-        int top = f(i-1, j, dp);
-        int left = f(i, j-1, dp);
-        return dp[i][j] = top+left;
-    }
+    //tabulation ------------------
+    
+    // int f(int i, int j, vector<vector<int>>& dp){
+    //     if(i == 0 && j == 0)return 1;
+    //     if(i < 0 || j < 0){
+    //         return 0;
+    //     }
+    //     if(dp[i][j]!=-1)return dp[i][j];
+    //     int top = f(i-1, j, dp);
+    //     int left = f(i, j-1, dp);
+    //     return dp[i][j] = top+left;
+    // }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m, vector<int>(n, -1));
-        return f(m-1, n-1, dp);
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                }
+                else{
+                    int top = 0, left = 0;
+                    if(i >= 1)
+                    top = dp[i-1][j];
+                    if(j >= 1)
+                    left = dp[i][j-1];
+                    dp[i][j] = top+left;
+                }
+            }
+        }
+        // for(auto x : dp){
+        //     for(auto y : x){
+        //         cout << y << " ";
+        //     }
+        //     cout << "\n";
+        // }
+        return dp[m-1][n-1];
     }
 };
