f12aefb0500f4689dd996afb0ad41798509fcbae
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-06-23 11:10:49 +0530

    Time: 7 ms (35.10%), Space: 7.9 MB (12.47%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    // memoize 
+    // tabulation --
+    
+    
     int f(int i, int j, vector<vector<int>>& og, vector<vector<int>>& dp){
         if(i >= 0 && j >= 0 && og[i][j] == 1)return 0;
         if(i == 0 && j == 0)return 1;
@@ -14,6 +16,20 @@ public:
         int m = og.size();
         int n = og[0].size();
         vector<vector<int>>dp(m, vector<int>(n, -1));
-        return f(m-1 ,n-1 ,og, dp);
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(og[i][j] == 1){
+                    dp[i][j] = 0;
+                }else if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                }else{
+                    int top = 0, left = 0;
+                    if(i>=1)top = dp[i-1][j];
+                    if(j>=1)left = dp[i][j-1];
+                    dp[i][j] = top + left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
