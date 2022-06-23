2a22aa6fe6ae75e4fd054fb0e58324c6f287186c
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-06-23 13:41:11 +0530

    Time: 17 ms (34.81%), Space: 9.8 MB (76.32%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
-    // tabulation
+    
+    // SPAce Optimization NInja Technique
+    
+    
     // int f(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
     //     if(i == 0 && j == 0)return grid[i][j];
     //     if(i < 0 || j < 0)return 1e8;
@@ -12,23 +15,25 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>>dp(m, vector<int>(n, -1));
+        vector<int> prev(n, -1);
+        vector<int> curr(n, -1);
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
                 if(i == 0 && j == 0){
-                    dp[i][j] = grid[i][j];
+                    curr[j] = grid[i][j];
                 }else{
                     int top = 1e8, left = 1e8;
                     if(i>=1){
-                        top = grid[i][j] + dp[i-1][j];
+                        top = grid[i][j] + prev[j];
                     }
                     if(j>=1){
-                        left = grid[i][j] + dp[i][j-1];
+                        left = grid[i][j] + curr[j-1];
                     }
-                    dp[i][j] = min(top, left);
+                    curr[j] = min(top, left);
                 }
             }
+            prev = curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
