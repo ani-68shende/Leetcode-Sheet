c0cd8ffb243fcb2d7a06c9a32b55d81ea2cc3350
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-06-25 15:56:48 +0530

    Time: 63 ms (27.44%), Space: 18.4 MB (40.22%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-    // memoization
-    int f(int ind, int target, vector<int> nums, vector<vector<int>>& dp){
-        if(target == 0)return 1;
-        if(ind == 0){
-            if(target%nums[ind] == 0)return 1;
-            else return 0;
-        }
-        if(dp[ind][target]!=-1)return dp[ind][target];
-        int notTake = f(ind-1, target, nums, dp);
-        int take = 0;
-        if(target >= nums[ind]){
-            take = f(ind, target-nums[ind], nums, dp);
-        }
-        return dp[ind][target] = take+notTake;
-    }
+    
+    //tabulation
+    
+    
+    // int f(int ind, int target, vector<int> nums, vector<vector<int>>& dp){
+    //     if(target == 0)return 1;
+    //     if(ind == 0){
+    //         if(target%nums[ind] == 0)return 1;
+    //         else return 0;
+    //     }
+    //     if(dp[ind][target]!=-1)return dp[ind][target];
+    //     int notTake = f(ind-1, target, nums, dp);
+    //     int take = 0;
+    //     if(target >= nums[ind]){
+    //         take = f(ind, target-nums[ind], nums, dp);
+    //     }
+    //     return dp[ind][target] = take+notTake;
+    // }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>>dp(n, vector<int>(amount+1,-1));
-        return f(-1+n, amount, coins, dp);
+        int target = amount;
+        vector<vector<int>>dp(n, vector<int>(amount+1,0));
+        for(int j = 0 ; j <= target ; j++){
+            if(j%coins[0] == 0)
+                dp[0][j] = 1;
+        }
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 0 ; j <= target ; j++){
+                int notTake = dp[i-1][j];
+                int take = 0;
+                if(j >= coins[i]){
+                    take = dp[i][j-coins[i]];
+                }
+                dp[i][j] = take+notTake;
+            }
+        }
+        return dp[n-1][target];
     }
 };
