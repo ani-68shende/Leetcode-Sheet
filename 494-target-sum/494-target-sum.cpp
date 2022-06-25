b8665f867471895848ec6b02bcd209f17df1fb1a
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-06-25 08:52:41 +0530

    Time: 1632 ms (9.87%), Space: 8.9 MB (93.89%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    // 
+    int f(int ind, vector<int>& nums, int sum, int target){
+        if(ind < 0 && target == sum)return 1;
+        if(ind < 0)
+             return 0;
+        int notTake = f(ind-1, nums, sum+nums[ind], target);
+        int take = f(ind-1, nums, sum-nums[ind], target);
+        return notTake+take;
+    }
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = 0;
+        if(nums.size() == 1 &&  abs(nums[0]) == abs(target))return 1;
+        return f(nums.size()-1, nums, sum, target);
+    }
+};
