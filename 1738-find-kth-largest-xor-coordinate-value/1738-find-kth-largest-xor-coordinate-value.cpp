fd0b7322aa36e4adc0f03606174893da37dd1aba
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-06-25 10:22:34 +0530

    Time: 824 ms (61.69%), Space: 121.1 MB (12.69%) - LeetHub

---
+++ 1738-find-kth-largest-xor-coordinate-value/1738-find-kth-largest-xor-coordinate-value.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int kthLargestValue(vector<vector<int>>& matrix, int k) {
+        priority_queue<long, vector<long>, greater<long>>pq;
+        vector<vector<int>>v(matrix);
+        int n = matrix.size();
+        int m = matrix[0].size();
+        for(int i = 1 ; i < m ; i++){
+            v[0][i] = v[0][i-1] ^ v[0][i];
+        }
+        for(int i = 1 ; i < n ; i++){
+            v[i][0] = v[i-1][0] ^ v[i][0];
+        }
+        for(int i = 1; i < n ; i++){
+            for(int j = 1 ; j < m ; j++){
+                v[i][j] = v[i-1][j] ^ v[i][j-1] ^ v[i-1][j-1] ^ v[i][j];
+            }
+        }
+        for(int i = 0; i < n ; i++){
+            for(int j = 0; j < m ; j++){
+                pq.push(v[i][j]);
+                if(pq.size()>k){
+                    pq.pop();
+                }
+            }
+        }
+        return pq.top();
+    }
+};
