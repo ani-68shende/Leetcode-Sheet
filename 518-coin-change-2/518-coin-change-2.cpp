49e6d546c59615c69eaac9c0c5f43e116ee1f5a9
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-06-25 15:49:51 +0530

    Time: 192 ms (6.88%), Space: 300 MB (5.00%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    //recursive
+    int f(int ind, int target, vector<int> nums, vector<vector<int>>& dp){
+        if(target == 0)return 1;
+        if(ind == 0){
+            if(target%nums[ind] == 0)return 1;
+            else return 0;
+        }
+        if(dp[ind][target]!=-1)return dp[ind][target];
+        int notTake = f(ind-1, target, nums, dp);
+        int take = 0;
+        if(target >= nums[ind]){
+            take = f(ind, target-nums[ind], nums, dp);
+        }
+        return dp[ind][target] = take+notTake;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>>dp(n, vector<int>(amount+1,-1));
+        return f(-1+n, amount, coins, dp);
+    }
+};
