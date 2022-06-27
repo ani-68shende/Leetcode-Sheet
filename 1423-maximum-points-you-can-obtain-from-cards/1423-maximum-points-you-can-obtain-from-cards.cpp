0bc1679e6e35eaee0a2e8dc09af94b5d8d0aacc7
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-06-27 07:33:34 +0530

    Time: 59 ms (96.38%), Space: 42.3 MB (96.60%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     // minimum subarray solves the problem :D
+    // be careful set the res = total because its the only max value possible
     int maxScore(vector<int>& cp, int k) {
         int i = 0;
-        int res = 1e5;
         int j = 0;
         int new_k = cp.size()-k;
         int sum = 0;
@@ -11,17 +11,17 @@ public:
         for(auto x : cp){
             total +=  x;
         }
-        res = total;
+        int res = 0;
         if(k == cp.size())return total;
         while(j < cp.size()){
             sum += cp[j];
             if(j - i + 1 == new_k){
-                res= min(res, sum);
+                res= max(res, total - sum);
                 sum -= cp[i];
                 i++;
             }
             j++;
         }
-        return total - res;
+        return res;
     }
 };
