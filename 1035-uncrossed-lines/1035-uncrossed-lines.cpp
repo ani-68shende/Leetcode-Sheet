688a96c4f975b1c36445310e54a68b512cb4f9eb
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-06-29 00:41:47 +0530

    Time: 31 ms (36.68%), Space: 13.1 MB (7.55%) - LeetHub

---
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& text1, vector<int>& text2) {
+        int n = text1.size(), m= text2.size(); 
+        vector<vector<int>> lookup(n+1, vector<int>(m+1, 0));
+        // for (int k = 0; k <= n; k++) lookup[k][0] = k;
+        // for (int k = 0; k <= m; k++) lookup[0][k] = k;
+        for(int i=1; i <= n; i++){
+            for(int j=1; j <= m; j++){
+                if(text1[i-1] == text2[j-1])
+                    lookup[i][j] = 1 + lookup[i-1][j-1];
+                else
+                    lookup[i][j] = max(lookup[i-1][j], lookup[i][j-1]);
+            }
+        }
+        return lookup[n][m];
+    }
+};
