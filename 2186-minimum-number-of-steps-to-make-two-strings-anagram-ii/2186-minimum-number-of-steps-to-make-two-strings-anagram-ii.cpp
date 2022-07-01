c7deebd292fdbbdd7b7c442a3e74b9ef470d0ea2
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-07-01 11:14:29 +0530

    Time: 158 ms (67.69%), Space: 29 MB (34.32%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -3,8 +3,6 @@ public:
     int minSteps(string s, string t) {
         int i = 0;
         int j = 0;
-        // sort(s.begin(), s.end());
-        // sort(t.begin(), t.end());
         int n = s.size();
         int m = t.size();
         int count = 0;
