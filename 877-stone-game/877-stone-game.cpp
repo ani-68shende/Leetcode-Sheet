88313d8aa26a7b2bfb6cbf29dd1d6e3905ab85e3
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-07-11 21:56:13 +0530

    Time: 952 ms (5.01%), Space: 116.3 MB (5.05%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
+    int f(int i, int j, vector<int>& piles, int alice, vector<vector<vector<int>>>& dp){
+        if(i > j){
+            return 0;
+        }
+        int val1, val2;
+        if(dp[i][j][alice]!=-1)return dp[i][j][alice];
+        //when alice is playing
+        if(alice){
+            val1 = piles[i] + f(i+1, j, piles, 0, dp);
+            val2 = piles[j] + f(i, j-1, piles, 0, dp);
+            return dp[i][j][alice] =  max(val1, val2);
+        }
+        
+        //when bob is playing
+        val1 = -piles[i] + f(i+1, j, piles, 1, dp);
+        val2 = -piles[j] + f(i, j-1, piles, 1, dp);
+        return dp[i][j][alice] = min(val1, val2);
+    }
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<int>>dp(n , vector<int>(n));
-        for(int g = 0 ; g < n ; g++){
-            for(int i = 0, j = g ; j < n ; i++, j++){
-                if(g == 0){
-                    dp[i][j] = piles[i];
-                }else if(g == 1){
-                    dp[i][j] = max(piles[i], piles[j]);
-                }else{
-                    int val1 = piles[i]+min(dp[i+1][j-1], dp[i+2][j]);
-                    int val2 = piles[j]+ min(dp[i+1][j-1], dp[i][j-2]);
-                    dp[i][j] = max(val1, val2);
-                }
-            }
-        }
-        return true;
+        int alice = 1;
+        int i = 0;
+        int j = n-1;
+        vector<vector<vector<int>>>dp(piles.size(), vector<vector<int>>(piles.size(), vector<int>(2,-1)));
+        int f2 = f(i, j, piles, alice, dp);
+        // cout << f2 << "\n";
+        return f2;
     }
 };
