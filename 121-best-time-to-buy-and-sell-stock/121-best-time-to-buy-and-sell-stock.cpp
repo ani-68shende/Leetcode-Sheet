1fb523b5b4219c362fd685aade2366ef237ae4ee
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-07-13 20:23:28 +0530

    Time: 214 ms (40.01%), Space: 93.3 MB (52.30%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -5,8 +5,8 @@ public:
         int ans = 0;
         int mini = prices[0];
         for(int i = 0 ; i < prices.size() ; i++){
-            ans = max(ans, prices[i]-mini);
             mini = min(mini, prices[i]);
+            ans = max(ans, prices[i]-mini);
         }
         return ans;
     }
