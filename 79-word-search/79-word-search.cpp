3cb82b359904ec8441ee3922aca120c86de5ffb1
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-07-15 21:02:25 +0530

    Time: 480 ms (58.34%), Space: 8.1 MB (35.71%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int n, m, vv;
-    bool dfs(int i, int j, vector<vector<char>>& board, string word){
-        if(word.size() == 0)return true;
-        if(i < 0 || j < 0 || i >= n || j >= m || word[0] != board[i][j]){
+    bool dfs(int i, int j, vector<vector<char>>& board, string &word, string& temp, int idx){
+        if(temp == word)return true;
+        if(temp.size() > min(vv, 15) || i < 0 || j < 0 || i >= n || j >= m || board[i][j] != word[idx]){
             return false;
         }
         char ch = board[i][j];
-        string new_word = word.substr(1);
+        temp.push_back(ch);
         board[i][j] = '$';
-        bool flag = dfs(i + 1, j, board, new_word) || dfs(i, j + 1, board, new_word) || dfs(i, j - 1, board, new_word) || dfs(i - 1, j, board, new_word);
+        bool flag = dfs(i + 1, j, board, word, temp, idx+1) || dfs(i, j + 1, board, word, temp, idx+1) || dfs(i, j - 1, board, word, temp, idx+1) || dfs(i - 1, j, board, word, temp, idx+1);
+        //temp.pop_back();
+        temp.pop_back();
         board[i][j] = ch;
         return flag;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        vv = word.size();
         bool g = false;
+        vv = word.size();
         n = board.size();
         m = board[0].size();
+        string temp = "";
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(dfs(i, j, board, word))return true;
+                if(dfs(i, j, board, word, temp, 0))return true;
             }
         }
         return false;
