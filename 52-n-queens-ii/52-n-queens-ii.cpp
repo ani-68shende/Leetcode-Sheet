92993405ea134eb4fcec886dbe15eab81242d0e0
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-07-24 01:59:32 +0530

    Time: 73 ms (10.50%), Space: 45.2 MB (6.05%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isValid(int row, int col, vector<string> board){
+        int r = row;
+        int c = col;
+        int n = board.size();
+        while(r >= 0 && c >=0){
+            if(board[r][c] == 'Q')return false;
+            r--;
+            c--;
+        }
+        r = row;
+        c = col;
+        while(r < n && c >= 0){
+            if(board[r][c] == 'Q')return false;
+            r++;
+            c--;
+        }
+        r = row;
+        c = col;
+        while(c >= 0){
+            if(board[r][c] == 'Q')return false;
+            c--;
+        }
+        return true;
+    }
+    void solve(int col, vector<string>&board, int n, vector<vector<string>>& ans){
+        if(col == n){
+            ans.push_back(board);
+            return ;
+        }
+        for(auto i = 0 ; i < n ; i++){
+            if(isValid(i, col, board)){
+                board[i][col] = 'Q';
+                solve(col+1, board, n, ans);
+                board[i][col] = '.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        vector<string>board(n);
+        string ss(n, '.');
+        for(int i = 0 ; i < n ; i++){
+            board[i] = ss;
+        }
+        vector<vector<string>>ans;
+        solve(0, board, n, ans);    
+        return ans.size();
+    }
+};
