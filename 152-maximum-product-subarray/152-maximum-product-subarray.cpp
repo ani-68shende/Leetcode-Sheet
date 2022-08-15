595e35bcf7218502cbb4c69a042461b3ef108a43
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-08-15 11:36:39 +0530

    Time: 7 ms (87.53%), Space: 13.7 MB (94.80%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
+    // v2
     int maxProduct(vector<int>& nums) {
         int maxu = 0;
         int n = nums.size();
         if(n == 1)return nums[0];
-        vector<int>pref;
-        vector<int>suff;
-        long long int mul = nums[0];
-        if(mul == 0) mul = 1;
-        pref.push_back(nums[0]);
-        for(int i = 1 ; i < n ; i++){
+        long long maxuu = INT_MIN;
+        long long int mul = 1;
+        for(int i = 0 ; i < n ; i++){
             if(nums[i] == 0){
-                pref.push_back(nums[i]);
                 mul = 1;
+                maxuu = max(maxuu, 0*1LL);
                 continue;
             }
             mul *= nums[i];
-            pref.push_back(mul);
+            maxuu = max(maxuu, mul);
         }
         reverse(nums.begin(),nums.end());
-        suff.push_back(nums[0]);
-        mul = nums[0];
-        if(mul == 0)mul = 1;
-        for(int i = 1 ; i < n ; i++){
+        mul = 1;
+        for(int i = 0 ; i < n ; i++){
             if(nums[i] == 0){
-                suff.push_back(nums[i]);
                 mul = 1;
+                maxuu = max(maxuu, 0*1LL);
                 continue;
             }
             mul *= nums[i];
-            suff.push_back(mul);
-        }
-        long long maxuu = INT_MIN;
-        for(auto x : pref){
-            // cout << x << " ";
-            maxuu = max(maxuu, x*1LL);
-        }
-        for(auto x : suff){
-            // cout << x << " ";
-            maxuu = max(maxuu, x*1LL);
+            maxuu = max(maxuu, mul);
         }
         return maxuu;
     }
