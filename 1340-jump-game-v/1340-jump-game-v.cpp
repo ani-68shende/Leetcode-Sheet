d21ebe6b6a1fee5b36577bcc3910cd69a876829f
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-08-16 23:33:28 +0530

    Time: 95 ms (21.25%), Space: 15.1 MB (34.83%) - LeetHub

---
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int f(int prev, int i, int d, vector<int>& arr, vector<int> & dp){
+        if(dp[i]!=-1)return dp[i];
+        int maxi = 0;
+        for(int k = i-1 ; k >= i-d ; k--){
+            if(k < 0 || k >=arr.size())continue;
+            if(prev > arr[k]){
+                maxi = max(maxi, 1+f(arr[k], k, d, arr, dp));   
+            }else{
+                break;
+            }
+        }
+        for(int k = i+1 ; k <= i+d ; k++){
+            if(k < 0 || k >=arr.size())continue;
+            if(prev > arr[k]){
+                maxi = max(maxi, 1+f(arr[k], k, d, arr, dp));   
+            }else{
+                break;
+            }
+        }
+        return dp[i] = maxi;
+    }
+    int maxJumps(vector<int>& arr, int d) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int maxu = 0;
+        vector<int>dp(arr.size(), -1);
+        for(int i = 0 ; i < arr.size() ; i++){
+            maxu = max(maxu, f(arr[i], i, d, arr, dp));
+        }
+        return maxu+1;
+    }
+};
