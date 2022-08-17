05ff0a4420c090cf124cf1b4b472320bfee96675
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-08-17 12:09:33 +0530

    Time: 30 ms (35.95%), Space: 17.1 MB (12.00%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    // recursive
+    int home_rob(int i, vector<int> &v, vector<int> & dp){
+        if(i == 0)return v[i];
+        if(i < 0)return 0;
+        if(dp[i]!=-1)return dp[i];
+        int notpick = 0 + home_rob(i-1, v, dp);
+        int pick = v[i] + home_rob(i-2, v, dp);
+        return dp[i] = max(pick, notpick);
+    }
+    int deleteAndEarn(vector<int>& nums) {
+        // creation takes time so please wait
+        int maxu = *max_element(nums.begin(), nums.end());
+        vector<int>v(10001, 0), dp(10001, -1);
+        for(int i(0) ; i < nums.size() ; i++){
+            v[nums[i]] += nums[i];
+        }
+        return home_rob(10000, v, dp);
+    }
+};
