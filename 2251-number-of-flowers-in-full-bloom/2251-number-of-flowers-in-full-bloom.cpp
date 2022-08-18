b2cad8f84c1c4596e4807ce3af7c562a2f1f6e42
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-08-18 12:27:22 +0530

    Time: 488 ms (59.78%), Space: 92.6 MB (29.40%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -4,18 +4,27 @@ public:
     //dont know how this works if you understand plz let me know
     
      vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& persons) {
-        vector<int> start, end;
-        for (auto& t : flowers)
-            start.push_back(t[0]), end.push_back(t[1]);
-        sort(start.begin(), start.end());
-        sort(end.begin(), end.end());
-        vector<int> res;
-        for (int t : persons) {
-            int started = upper_bound(start.begin(), start.end(), t) - start.begin();
-            int ended = lower_bound(end.begin(), end.end(), t) - end.begin();
-            // cout << started << " " << ended << endl;
-            res.push_back(started - ended);
+        map<int,int>m;
+        for(auto it:flowers){
+            m[it[0]]++;
+            m[it[1]+1]--;
+        }
+        int sum=0;
+        for(auto &it:m){
+            sum+=it.second;
+            it.second=sum;
+        }
+        int n=persons.size();
+        vector<int>res(n,0);
+        for(int i=0;i<n;i++){
+            auto it=m.upper_bound(persons[i]);
+            if(it!=m.begin()){
+                // cout << (*it).first << " " << (*it).second << endl;
+                it--;
+                res[i]=it->second;
+            }
         }
         return res;
     }
+
 };
