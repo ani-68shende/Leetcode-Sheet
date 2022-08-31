6273ea16aa730105c26c284560aaeb99779bacfd
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-08-31 19:38:27 +0530

    Time: 36 ms (99.43%), Space: 32.2 MB (65.82%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -5,8 +5,7 @@ public:
         if(i < 0 || i >= arr.size())return false;
         if(visited[i])return false;
         visited[i] = 1;
-        if(i >= 0 && i < arr.size()){
-            if(arr[i] == 0)
+        if(i >= 0 && i < arr.size() && arr[i] == 0){
                 return true;
         }
         bool left = f(i-arr[i], arr);
