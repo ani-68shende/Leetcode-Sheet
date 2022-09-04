ff5f7c64666077180ae877f804e85e4889f842cf
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-09-05 00:28:40 +0530

    Time: 2 ms (50.03%), Space: 5.8 MB (89.49%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    long long int maxu = 0;
+    long long int dp[60];
     long long int f(int n){
         if(n == 0)return 1;
         if(n <= 1){
             return 0;
         }
+        if(dp[n]!=-1)return dp[n];
         long long int teo = 2*f(n-2);
         long long int thr = 3*f(n-3);
-        return max(teo, thr);
+        return dp[n] = max(teo, thr);
     }
     int integerBreak(int n) {
+        memset(dp, -1, sizeof(dp));
         if(n == 2 || n == 3)return n-1;
         return f(n);
     }
