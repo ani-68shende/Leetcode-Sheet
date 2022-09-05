66fd4311cb07e238cf12a3e2ba9e45c5d3ad97a0
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-09-06 00:08:29 +0530

    Time: 19 ms (67.24%), Space: 9.5 MB (45.34%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     int minFlips(string target) {
-        int count =0;
+        int count = 0;
         char prev = '0';
-        // reverse(target.begin(), target.end());
-        // int j = -1 + target.size();
-        // while(j >= 0 && target[j]=='0'){
-        //     target.pop_back();
-        //     j--;
-        // }
-        reverse(target.begin(), target.end());
-        for(int i = target.size()-1; i >= 0 ; i--){
+        for(int i = 0; i < target.size() ; i++){
             if(prev!=target[i]){
                 prev = target[i];
                 count++;
