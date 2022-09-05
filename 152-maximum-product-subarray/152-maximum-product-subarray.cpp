49b942f7f0117fbce84cc04d4d41da70b0e76def
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-09-05 14:39:42 +0530

    Time: 9 ms (71.07%), Space: 13.8 MB (71.27%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -2,31 +2,18 @@ class Solution {
 public:
     // v2
     int maxProduct(vector<int>& nums) {
-        int maxu = 0;
-        int n = nums.size();
-        if(n == 1)return nums[0];
-        long long maxuu = INT_MIN;
-        long long int mul = 1;
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i] == 0){
-                mul = 1;
-                maxuu = max(maxuu, 0*1LL);
-                continue;
-            }
-            mul *= nums[i];
-            maxuu = max(maxuu, mul);
+        int maxu = INT_MIN, n = nums.size(), prod = 1;
+        for(int i = 0; i < n ; i++){
+            prod *= nums[i];
+            maxu = max(maxu, prod);
+            if(prod == 0)prod = 1;
         }
-        reverse(nums.begin(),nums.end());
-        mul = 1;
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i] == 0){
-                mul = 1;
-                maxuu = max(maxuu, 0*1LL);
-                continue;
-            }
-            mul *= nums[i];
-            maxuu = max(maxuu, mul);
+        prod = 1;
+        for(int i = n-1; i >= 0 ; i--){
+            prod *= nums[i];
+            maxu = max(maxu, prod);
+            if(prod == 0)prod = 1;
         }
-        return maxuu;
+        return maxu;
     }
 };
