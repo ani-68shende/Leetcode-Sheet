822bc478189e47705422531f0ec4f4af552af55c
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-09-11 00:52:45 +0530

    Time: 29 ms (39.29%), Space: 5.8 MB (72.80%) - LeetHub

--- 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
@@ -1,20 +1,5 @@
 class Solution {
 public:
-//     long long nCr(long long n, long long r)
-//     {
-//         return fact(n) / (fact(r) * fact(n - r));
-//     }
-
-//     // Returns factorial of n
-//     long long fact(long long n)
-//     {
-//         if(n==0)
-//         return 1;
-//         long long res = 1;
-//         for (long long i = 2; i <= n; i++)
-//             res = res * i;
-//         return res;
-//     }
     long long waysToBuyPensPencils(int total, int pen, int pencil) {
         long long count(0);
         int j = 0;
