ed2333a5558db646fb238ef3b16ec6791973fc3e
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-09-13 10:12:21 +0530

    Time: 7 ms (55.68%), Space: 11.2 MB (25.76%) - LeetHub

---
+++ 969-pancake-sorting/969-pancake-sorting.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> pancakeSort(vector<int>& arr) {
+        int count = 0;
+        int n = arr.size();
+        vector<int>vec;
+        for(int i = 0 ; i < n ; i++){
+            int maxu = 0;
+            int d = 0;
+            for(int j = 0 ; j < n-i ; j++){
+                if(arr[j]>maxu){
+                    maxu = arr[j];
+                    d = j;
+                }
+            }
+            reverse(arr.begin(), arr.begin()+d+1);
+            vec.push_back(d+1);
+            reverse(arr.begin(), arr.begin()+n-i);
+            vec.push_back(n-i);
+        }
+        return vec;
+    }
+};
