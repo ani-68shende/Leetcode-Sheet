c6effdacb5f98f0ae13b11551d1a2afce1ed96b5
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-09-14 14:38:34 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (93.88%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/556-next-greater-element-iii.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     int nextGreaterElement(int n) {
         string s = to_string(n);
-        string temp(s);
-        sort(temp.begin(), temp.end(), greater<char>());
-        if(temp == s)return -1;
         next_permutation(s.begin(), s.end());
         int num = stoll(s);
-        return num < n ? -1 : num;
+        return num <= n ? -1 : num;
     }
 };
