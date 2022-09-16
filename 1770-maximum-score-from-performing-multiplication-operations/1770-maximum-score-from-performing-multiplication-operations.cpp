b7ab936dfd02b6a28947db8372ac48d4a3f57be4
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-09-16 10:53:08 +0530

    Time: 950 ms (19.50%), Space: 334.3 MB (6.28%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    int dp[1000][1000];
-
-int answer_dp(vector<int> &nums, vector<int> &multi, int i, int l, int r){
-
-	if(i==multi.size())
-		return 0;
-
-	if(dp[l][i]!=-1)
-		return dp[l][i];
-
-	int first = multi[i]*nums[l] + answer_dp(nums, multi, i+1, l+1, r);
-	int last = multi[i]*nums[r] + answer_dp(nums, multi, i+1, l, r-1);
-
-	dp[l][i] = max(first, last);
-
-	return dp[l][i];
-}
-int maximumScore(vector<int>& nums, vector<int>& multi) {
-
-    //jugaaaaaaaaaadaaddddddd
-    int sum1 = 0;
-    int sum2= 0;
-    for(auto x : nums)sum1+=x;
-    for(auto y : multi)sum2+=y;
-    if(sum1 == -1 && sum2 == multi.size())return -1;
-    ////tilllllllllllaaaa ahhhhehrrreeere
-    memset(dp, -1, sizeof(dp));
-    return answer_dp(nums, multi, 0, 0, nums.size()-1);
-}
+    vector<vector<int>> kgf;
+    int f(int l, int i, vector<int>&nums, vector<int>& multipliers){
+        if(i == multipliers.size())return 0;
+        if(kgf[l][i]!=INT_MAX)return kgf[l][i];
+            
+        int r = nums.size()-1-(i-l);
+        int left =  nums[l] * multipliers[i] + f(l+1, i+1, nums, multipliers);
+        int right = nums[r] * multipliers[i] + f(l, i+1, nums, multipliers);
+        // no need of r =>due to move
+        return kgf[l][i] = max(left, right);
+    }
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        int n = nums.size();
+        int m = multipliers.size();
+        kgf.resize(1001, vector<int>(1001, INT_MAX));
+        return f(0, 0, nums, multipliers);
+    }
 };
