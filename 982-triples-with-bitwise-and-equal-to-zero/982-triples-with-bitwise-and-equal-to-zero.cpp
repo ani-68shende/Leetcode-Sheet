1231bcabfc6cb4524370df9ce70529ef5d8ebd48
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-09-23 10:38:21 +0530

    Time: 595 ms (58.56%), Space: 13 MB (30.95%) - LeetHub

---
+++ 982-triples-with-bitwise-and-equal-to-zero/982-triples-with-bitwise-and-equal-to-zero.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countTriplets(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+        int count = 0;
+        int n = nums.size();
+        unordered_map<int, int>mp;
+        for(auto a : nums){
+            for(auto b : nums){
+                mp[a&b]++;
+            }
+        }
+        for(auto a: nums){
+            for(auto x : mp){
+                if((a&x.first)==0)
+                    count+=x.second;
+            }
+        }
+        return count;
+    }
+};
