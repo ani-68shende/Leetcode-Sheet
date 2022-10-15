a0919154225e111645b7bc199daf259ee2ade56b
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-10-15 15:07:27 +0530

    Time: 108 ms (63.67%), Space: 28.9 MB (22.45%) - LeetHub

--- 2223-sum-of-scores-of-built-strings/2223-sum-of-scores-of-built-strings.cpp
+++ 2223-sum-of-scores-of-built-strings/2223-sum-of-scores-of-built-strings.cpp
@@ -15,21 +15,12 @@ public:
         int n = str.length();
         int L, R, k;
         vector<long long>pajaz(str.size());
-        // [L,R] make a window which matches with prefix of s
         L = R = 0;
         for (int i = 1; i < n; ++i)
         {
-            // if i>R nothing matches so we will calculate.
-            // Z[i] using naive way.
             if (i > R)
             {
                 L = R = i;
-
-                // R-L = 0 in starting, so it will start
-                // checking from 0'th index. For example,
-                // for "ababab" and i = 1, the value of R
-                // remains 0 and Z[i] becomes 0. For string
-                // "aaaaaa" and i = 1, Z[i] and R become 5
                 while (R<n && str[R-L] == str[R])
                     R++;
                 pajaz[i] = R-L;
@@ -37,22 +28,11 @@ public:
             }
             else
             {
-                // k = i-L so k corresponds to number which
-                // matches in [L,R] interval.
                 k = i-L;
-
-                // if Z[k] is less than remaining interval
-                // then Z[i] will be equal to Z[k].
-                // For example, str = "ababab", i = 3, R = 5
-                // and L = 2
                 if (pajaz[k] < R-i+1)
                     pajaz[i] = pajaz[k];
-
-                // For example str = "aaaaaa" and i = 2, R is 5,
-                // L is 0
                 else
                 {
-                    // else start from R and check manually
                     L = i;
                     while (R<n && str[R-L] == str[R])
                         R++;
