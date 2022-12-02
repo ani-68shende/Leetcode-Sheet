5d49e20e69925e20137a2274edeb71278bac63cd
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-12-02 10:21:06 +0530

    Time: 7 ms (87.89%), Space: 9 MB (30.52%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void dfs(Node* node, Node* copy, vector<Node*>& visited){
+        visited[copy->val] = copy;
+        for(Node *child : node->neighbors){
+            if(visited[child->val] == NULL){
+                Node* temp = new Node(child->val);
+                copy->neighbors.push_back(temp);
+                dfs(child, temp, visited);
+            }else{
+                // ye smajhao bhai koi 
+                copy->neighbors.push_back(visited[child->val]);
+            }
+        }
+    }
+    Node* cloneGraph(Node* node) {
+        if(node == NULL)return node;
+        Node* copy = new Node(node->val);
+        vector<Node* > visited(101, NULL);
+        dfs(node, copy, visited);
+        return copy;
+    }
+};
