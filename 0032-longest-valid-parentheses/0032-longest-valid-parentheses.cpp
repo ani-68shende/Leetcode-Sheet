c8e8f9304175f2211cae239d63f1c36ff3f7586f
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2022-12-02 10:19:03 +0530

    Time: 8 ms (61.50%), Space: 7.3 MB (43.09%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    // pushing -1 is the key here,,, and as we need the length one more hint is to concentrate on index...
+    //prajjwa reading this ik
+    int longestValidParentheses(string s) {
+        stack<int> stk;
+        stk.push(-1);
+        int maxL=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(stk.top()!=-1&&s[i]==')'&&s[stk.top()]=='(')
+            {
+                // chal hat aisa karke nikala isko pop or what()
+                stk.pop();
+                maxL=max(maxL,i-stk.top());
+            }
+            else
+                stk.push(i);
+        }
+        return maxL;
+    }
+};
