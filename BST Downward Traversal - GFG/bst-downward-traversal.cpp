95f1857f76e24bd8ef08558e882355290600bbfa
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2023-02-06 22:13:07 +0530

    Added solution - LeetHub

---
+++ BST Downward Traversal - GFG/bst-downward-traversal.cpp
@@ -0,0 +1,162 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+/*
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution{
+public:
+    long long int sum(Node* root,int dist){/*returns the sum of the Nodes vertically below the targetNode*/
+        if(!root) return 0;
+        long long int left=sum(root->left,dist-1);
+        long long int right=sum(root->right,dist+1);
+        return (dist==0)?left+right+root->data:left+right;
+    }
+    Node* search(Node* root,int target){/*searches for the node whose value is equal to target*/
+        while(root){
+            if(root->data==target){
+                return root;
+            }
+            else if(root->data<target){
+                root=root->right;
+            }
+            else{
+                root=root->left;
+            }
+        }
+        return NULL;
+    }
+    long long int verticallyDownBST(Node *root,int target){
+        Node* targetNode=search(root,target);
+        if(targetNode==NULL) return -1;
+        return sum(targetNode,0)-targetNode->data;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+void inorder(Node *root, vector<int> &v)
+{
+    if(root==NULL)
+        return;
+
+    inorder(root->left, v);
+    v.push_back(root->data);
+    inorder(root->right, v);
+}
+
+int main() {
+ 
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+        int target;
+        cin>>target;
+        string newline;
+        getline(cin,newline);
+        string s; 
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        cout<<ob.verticallyDownBST(root, target)<<endl;
+   }
+   return 0;
+}
+// } Driver Code Ends
