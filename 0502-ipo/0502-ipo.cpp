b88e3bf65572ed9e0bb2627196189640bd7bda7b
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2023-02-26 16:19:01 +0530

    Time: 291 ms (16.80%), Space: 95.5 MB (6.71%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,22 +1,6 @@
 #define ll long long int
 class Solution {
 public:
-    ll find(vector<pair<ll, ll>>&v, ll target){
-        ll low = 0;
-        ll high = v.size() - 1;
-        if(v[v.size()-1].first <= target)return v.size()-1;
-        ll ans = -1;
-        while(low <= high){
-            ll mid = low + (high - low)/2;
-            if(v[mid].first <= target){
-                ans = mid;
-                low = mid + 1;
-            }else{
-                high = mid - 1;
-            }
-        }
-        return ans;
-    }
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
         vector<pair<ll, ll>>v;
         ll n = profits.size();
@@ -33,12 +17,7 @@ public:
             while(j < n && v[j].first <= ini){
                 pq.push({v[j].second, v[j].first});
                 j++;
-                if(j == n){
-                    flag = 1;
-                    break;
-                }
             }
-            if(flag == 1)break;
             if(pq.size()){
                 // cout << "yes\n";
                 ini += pq.top().first;
