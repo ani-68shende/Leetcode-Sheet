251305b33b5500c75d4024df1079885b63b29eec
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2023-03-29 00:25:52 +0530

    Time: 8 ms (30.37%), Space: 9.4 MB (91.47%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -9,17 +9,27 @@ public:
         if(dp[i]!=-1)return dp[i];
         int take1 = INT_MAX, take2 = INT_MAX, take3 = INT_MAX;
         int ini = days[i];
-        auto i1 = lower_bound(days.begin(), days.end(), days[i]+30) - days.begin();
+        auto i1 = upper_bound(days.begin(), days.end(), days[i]+29) - days.begin();
         take1 = three + ff(i1, days);
-        auto i2 = lower_bound(days.begin(), days.end(), days[i]+7) - days.begin();
+        auto i2 = upper_bound(days.begin(), days.end(), days[i]+6) - days.begin();
         take2 = two + ff(i2, days);
         take3 = one + ff(i+1, days);
         return dp[i]=min({take1, take2, take3});
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         n = days.size();
-        memset(dp, -1, sizeof(dp));
+        memset(dp, 0, sizeof(dp));
         one = costs[0], two = costs[1], three = costs[2];
-        return ff(0, days);
+        for(int i = n-1 ; i >= 0 ; i--){
+            int take1 = INT_MAX, take2 = INT_MAX, take3 = INT_MAX;
+            int ini = days[i];
+            auto i1 = upper_bound(days.begin(), days.end(), days[i]+29) - days.begin();
+            take1 = three + dp[i1];
+            auto i2 = upper_bound(days.begin(), days.end(), days[i]+6) - days.begin();
+            take2 = two + dp[i2];
+            take3 = one + dp[i+1];
+            dp[i]=min({take1, take2, take3});
+        }
+        return dp[0];
     }
 };
