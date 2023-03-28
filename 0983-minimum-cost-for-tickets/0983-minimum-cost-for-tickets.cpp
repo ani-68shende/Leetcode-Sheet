0b6da2f65f2664e20641f47596b4a7338ddf234a
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2023-03-28 07:36:43 +0530

    Time: 9 ms (22.34%), Space: 9.3 MB (97.49%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int one, two, three, n;
+    int dp[366];
+    int ff(int i, vector<int>& days){
+        if(i >= days.size()){
+            return 0;
+        }
+        if(dp[i]!=-1)return dp[i];
+        int take1 = INT_MAX, take2 = INT_MAX, take3 = INT_MAX;
+        int ini = days[i];
+        auto i1 = upper_bound(days.begin(), days.end(), days[i]+29) - days.begin();
+        take1 = three + ff(i1, days);
+        auto i2 = upper_bound(days.begin(), days.end(), days[i]+6) - days.begin();
+        take2 = two + ff(i2, days);
+        take3 = one + ff(i+1, days);
+        return dp[i]=min({take1, take2, take3});
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        n = days.size();
+        memset(dp, -1, sizeof(dp));
+        one = costs[0], two = costs[1], three = costs[2];
+        return ff(0, days);
+    }
+};
