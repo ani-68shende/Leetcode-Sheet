2ab4b7d30711f63f538b3eb4125f0ef883209fcb
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2023-03-29 09:06:14 +0530

    Time: 86 ms (5.11%), Space: 94.1 MB (6.44%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    int f(int i, vector<int>& ss, int time, vector<vector<int>>& dp){
-        if(i < 0)return 0;
-        if(dp[i][time]!=-1)return dp[i][time];
-        int notTake = f(i-1, ss, time, dp);
-        int take = ss[i]*time + f(i-1, ss, time+1, dp);
-        return dp[i][time] = max(take, notTake);
-    }
+    //see previous submission for recur + memo 
     int maxSatisfaction(vector<int>& ss) {
         sort(ss.begin(), ss.end(), greater<int>());
-        vector<vector<int>> dp(ss.size(), vector<int>(ss.size()+1, -1));
-        return f(ss.size()-1, ss, 1, dp);
+        vector<vector<int>> dp(501, vector<int>(505, 0));
+        for(int i = 0 ; i < ss.size() ; i++){
+            for(int time = ss.size() ; time >= 1 ; time--){
+                int notTake = dp[i][time];
+                int take = ss[i]*time + dp[i][time+1];
+                dp[i+1][time] = max(take, notTake);
+            }
+        }
+        return dp[ss.size()][1];
     }
 };
