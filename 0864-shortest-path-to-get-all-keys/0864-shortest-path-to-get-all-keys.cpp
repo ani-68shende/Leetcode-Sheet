786e9856ee3482fa7c2c63fd65d48db0526dc22e
ani-68shende <80747469+ani-68shende@users.noreply.github.com>
2023-06-29 18:20:53 +0530

    Time: 184 ms (35.49%), Space: 23.7 MB (47.10%) - LeetHub

--- 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.cpp
+++ 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.cpp
@@ -1,76 +1,94 @@
 class Solution {
 public:
-    int shortestPathAllKeys(vector<string>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        // getting the key lists
-        unordered_map<char, int> key_bit;
-        int bit_start = 0;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (islower(grid[i][j]))
-                    key_bit[grid[i][j]] = bit_start++;
+    int shortestPathAllKeys(vector<string>& grid)
+    {
+        int m = grid.size() , n = grid[0].size() ;
         
-        int mask_end = (1 << bit_start) - 1;
-        int mask_size = (1 << bit_start);
-
-        // preparing memo vector
-        vector<vector<vector<bool>>> memo(m,vector<vector<bool>>(n, 
-                                        vector<bool>(mask_size,false)));
-
-        // get the starting point
-        vector<int> start;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (grid[i][j] == '@')
-                    start = {i,j,0}; // 0 denoting no key state
-
-        // BFS Loop
-        queue<vector<int>> q;
-        q.push(start);
-        int step = 0;
-        while (!q.empty())
+        int r , c , k = 0 ;
+        
+        for(int i=0;i<m;i++)
         {
-            int size = q.size();
-            for (int k = 0; k < size; k++)
+            for(int j=0;j<n;j++)
             {
-                int row = q.front()[0];
-                int col = q.front()[1];
-                int mask = q.front()[2];
-                q.pop();
-
-                // check if out of bounds
-                if (row < 0 || row >= m || col < 0 || col >= n)
-                    continue;
-
-                // check if wall
-                if (grid[row][col] == '#') continue;
-
-                // check if locked and don't have key
-                if (isupper(grid[row][col]))
-                    if ((mask & (1 << key_bit[tolower(grid[row][col])])) == 0)
-                        continue;
-
-                // if a key is in current position, update state
-                if (islower(grid[row][col]))
-                    mask = mask | (1 << key_bit[grid[row][col]]);
-
-                // check if mask if complete
-                if (mask == mask_end) return step;
-
-                // check if state is already visited
-                if (memo[row][col][mask]) continue;
-                memo[row][col][mask] = true;
-
-                // moving to neighbouring cells
-                q.push({row+1, col, mask});
-                q.push({row-1, col, mask});
-                q.push({row, col+1, mask});
-                q.push({row, col-1, mask});
+                if(grid[i][j]=='@')
+                {
+                    r = i , c = j ;
+                }
+                
+				 // counting numbers of keys
+                if(grid[i][j]>='a' && grid[i][j]<='z')
+                    k++ ;
             }
-            step++;
         }
-        return -1;
+        
+        int mask = (1<<k)-1 ;
+        
+        map<int,set<int>> vis ;
+		// key = row*n + col ,  val is set of masks with which we have already visited the cell before.
+        // ith bit in mask is 1 implies we donot have its key yet. 0 implies we have the key
+        
+        queue<pair<int,int>> q ; //row*n+col , mask
+        
+        q.push({r*n+c,mask}) ;
+        vis[r*n+c].insert(mask) ;
+        
+        int dir[5] = {1,0,-1,0,1} ;
+        
+        int ans = 0 ;
+        
+        while(!q.empty())
+        {
+            int sz = q.size() ;
+            
+            while(sz--)
+            {
+                auto p = q.front() ; q.pop() ;
+            
+                int x = p.first/n , y = p.first%n ;
+                mask = p.second ;
+            
+                if(grid[x][y]>='a' && grid[x][y]<='z')
+                {
+                    if(mask&(1<<(grid[x][y]-'a')))
+                        mask-=(1<<(grid[x][y]-'a')) ;
+                }
+                
+                // we got all the k keys
+                if(mask==0)
+                    return ans ;
+                
+                for(int i=0;i<4;i++)
+                {
+                    int newx = x+dir[i] , newy = y+dir[i+1] ;
+                    
+					// new cell going out of grid
+                    if(newx<0 || newx>=m || newy<0 || newy>=n) 
+                        continue ;
+                    
+                    if(grid[newx][newy]=='#') // new cell is a wall
+                        continue ;
+                    
+					// new cell is lock and we don't have its key yet
+                    if(grid[newx][newy]>='A' && grid[newx][newy]<='Z' && (mask&(1<<(grid[newx][newy]-'A'))) )
+                    {
+                        continue ;
+                    }
+                    
+                    int key = (newx*n)+newy ;
+                
+				    // if the cell hasn't been visited with same set of keys
+                    if(vis[key].find(mask)==vis[key].end()) 
+                    {
+                        q.push({key,mask}) ;
+                        vis[key].insert(mask) ;
+                    }
+                }
+            }
+            
+            ans++ ;
+        }
+        
+        return -1 ;
+        
     }
 };
